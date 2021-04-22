eb6bfb135c3eafe567d3c895cba0b74b20e06851
yang <ysfnevercry@gmail.com>
2021-04-22 16:32:59 +0800

    更新页面

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
+    title: `nevercry's blog`,
+    name: `nevercry`,
+    siteUrl: `https://blog.nevercry.top`,
+    description: `nevercry's blog`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `My blog powered by Gatsby.`,
       maxWidth: 652,
     },
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/narative`,
+        url: `https://twitter.com/nevercry`,
       },
       {
         name: `github`,
-        url: `https://github.com/narative`,
-      },
-      {
-        name: `instagram`,
-        url: `https://instagram.com/narative.co`,
-      },
-      {
-        name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
-      },
+        url: `https://github.com/nevercry`,
+      }
     ],
   },
   plugins: [
